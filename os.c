d022c409a8f9c15d16abea0fd27704239de415eb
vedantshitale <43702444+vedantshitale@users.noreply.github.com>
2020-04-12 21:55:30 +0530

    Update os.c

--- os.c
+++ os.c
@@ -1,51 +1,53 @@
-#include<stdio.h>
-
-int main()
-{
-  float avwt;
-  int i,j,num,pt[20],wt[30],twt=0,pr[20],tmp1;
-  char pa[10][5],tmp[5];
-  printf("enter the no of process you want :-");
-  scanf("%d",&num);
-  for(i=0;i<num;i++)
-  {
-    printf("enter the process %d name:- ",i+1);
-    scanf("%s",&pa[i]);
-    printf("enter time required for process:- ");
-    scanf("%d",&pt[i]);
-    printf("enter priority:");
-    scanf("%d",&pr[i]);
-  }
- for(i=0;i<num-1;i++)
- {
- for(j=i+1;j<num;j++)
- {
-    if(pr[i]>pr[j])
- {
-   temp1=pr[i];
-   pr[i]=pr[j];
-   pr[j]=temp1;
-   temp1=pt[i];
-   pt[i]=pt[j];
-   pt[j]=temp1;
-   strcpy(temp,pa[i]);
-   strcpy(pa[i],pa[j]);
-   strcpy(pa[j],tmp);
-  }
-  }
-  }
- wt[0]=0;
-  for(i=1;i<n;i++)
-  {
-   wt[i]=wt[i-1]+p[i-1];
-   twt=twt+wt[i];
-  }   
-   avwt=(float)twt/num;   
-   printf("process_name\t process_time\t priority\t waiting_time\n");
-for(i=0;i<num;i++)  
-{
-   printf(" %s\t %d\t %d\t %d\n ",p[i],pt[i],pr[i],wt[i]);
-}
-  printf("total waiting time=%d\n avg waiting time=%f",tot,avg);
-  return 0;
+#include<stdio.h> 
+ 
+int main() 
+{ 
+ 
+  int count,j,n,time,remain,flag=0,time_quantum; 
+  int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10]; 
+  printf("Enter Total Process:\t "); 
+  scanf("%d",&n); 
+  remain=n; 
+  for(count=0;count<n;count++) 
+  { 
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
+    scanf("%d",&at[count]); 
+    scanf("%d",&bt[count]); 
+    rt[count]=bt[count]; 
+  } 
+  printf("Enter Time Quantum:\t"); 
+  scanf("%d",&time_quantum); 
+  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
+  for(time=0,count=0;remain!=0;) 
+  { 
+    if(rt[count]<=time_quantum && rt[count]>0) 
+    { 
+      time+=rt[count]; 
+      rt[count]=0; 
+      flag=1; 
+    } 
+    else if(rt[count]>0) 
+    { 
+      rt[count]-=time_quantum; 
+      time+=time_quantum; 
+    } 
+    if(rt[count]==0 && flag==1) 
+    { 
+      remain--; 
+      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]); 
+      wait_time+=time-at[count]-bt[count]; 
+      turnaround_time+=time-at[count]; 
+      flag=0; 
+    } 
+    if(count==n-1) 
+      count=0; 
+    else if(at[count+1]<=time) 
+      count++; 
+    else 
+      count=0; 
+  } 
+  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n); 
+  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n); 
+  
+  return 0; 
 }
